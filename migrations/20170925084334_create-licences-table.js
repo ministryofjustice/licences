2ad70a6ee4b377f6083a022c555135ec76a3f3d8
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-09-25 10:28:07 +0100

    Knex DB setup

---
+++ migrations/20170925084334_create-licences-table.js
@@ -0,0 +1,12 @@
+exports.up = knex =>
+    Promise.all([
+        knex.schema.createTableIfNotExists('LICENCES', table => {
+            table.increments('ID').primary('PK_LICENCE');
+            table.string('LICENCE').notNullable();
+        }),
+        knex.raw('ALTER TABLE [LICENCES] ALTER COLUMN [LICENCE] [NVARCHAR](MAX) NULL'),
+        knex.raw('ALTER TABLE [LICENCES] ADD CONSTRAINT [LICENCES.LICENCE should be formatted as JSON] CHECK (ISJSON(LICENCE) > 0)')
+    ]);
+
+exports.down = knex =>
+    knex.schema.dropTable('LICENCES');
