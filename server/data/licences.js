e9e159f310e524ae79a9dc267c57259022969376
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-09-27 11:20:38 +0100

    Unit tests for licence DB query

--- server/data/licences.js
+++ server/data/licences.js
@@ -5,13 +5,8 @@ const {resolveJsonResponse} = require('./dataAccess/azureJson');
 
 exports.getLicences = function(nomisIds) {
     return new Promise((resolve, reject) => {
-        const sql = `SELECT
-                        NOMIS_ID as nomisId,
-                        ID as id,
-                        JSON_QUERY(LICENCE) AS licence
-                     FROM LICENCES
-                     WHERE NOMIS_ID IN (${nomisIds.map(id=> `'${id}'`).join(',')})
-                     FOR JSON PATH`;
+        const sql = `SELECT NOMIS_ID as nomisId, ID as id, JSON_QUERY(LICENCE) AS licence
+                     FROM LICENCES WHERE NOMIS_ID IN (${nomisIds.map(id=> `'${id}'`).join(',')}) FOR JSON PATH`;
 
         getCollection(sql, null, resolveJsonResponse(resolve), reject);
     });
