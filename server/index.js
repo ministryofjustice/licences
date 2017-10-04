dfbd07d1e4ca6740d47c0147bcf45439a6a3f80d
mattwhitfield <whitfield.mj@gmail.com>
2017-10-04 13:15:11 +0100

    implemented discharge address route with dependency injection

--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ const logger = require('../log');
 
 const createReportService = require('./services/reportingInstructionsService');
 const createLicenceDetailsService = require('./services/licenceDetailsService');
+const createDischargeAddressService = require('./services/dischargeAddressService');
 
 // TODO inject API/DB dependencies into services
 const reportingInstructionService = createReportService();
 const licenceDetailsService = createLicenceDetailsService();
+const dischargeAddressService = createDischargeAddressService();
 
 const app = createApp({
     logger,
     reportingInstructionService,
-    licenceDetailsService
+    licenceDetailsService,
+    dischargeAddressService
 });
 
 module.exports = app;
