73259c635419d0ad687403e21ba736b6932b901c
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-10-17 12:48:19 +0100

    Add logging

--- server/services/prisonerDetailsService.js
+++ server/services/prisonerDetailsService.js
@@ -1,14 +1,20 @@
+const logger = require('../../log.js');
+
 module.exports = function createPrisonerDetailsService(nomisClient) {
     async function getPrisonerDetails(nomisId) {
         try {
+            logger.info(`getPrisonerDetail: ${nomisId}`);
 
             const bookings = await nomisClient.getBookings(nomisId);
+            logger.info(`got bookings size: ${bookings.length}`);
 
             const booking = bookings[0];
 
             const bookingDetail = await nomisClient.getBooking(booking.bookingId);
+            logger.info(`got booking detail for booking id: ${booking.bookingId}`);
 
             const image = await nomisClient.getImageInfo(booking.facialImageId);
+            logger.info(`got image detail for facialImageId id: ${booking.facialImageId}`);
 
             return {
                 dateOfBirth: booking.dateOfBirth,
@@ -27,7 +33,8 @@ module.exports = function createPrisonerDetailsService(nomisClient) {
 
         } catch (error) {
             // TODO more specific api failure handling
-            console.error('Error getting prisoner info');
+            logger.error('Error getting prisoner info');
+            logger.error(error);
             throw error;
         }
     }
