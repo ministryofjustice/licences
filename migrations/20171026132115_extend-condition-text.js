e69f9253a250e699c90ebcf2c1d81c158a9d581a
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-10-27 12:00:24 +0100

    Add missing rollback

--- migrations/20171026132115_extend-condition-text.js
+++ migrations/20171026132115_extend-condition-text.js
@@ -8,3 +8,11 @@ exports.up = knex =>
                 WITH (ONLINE = ON);`)
     ]);
 
+exports.down = knex =>
+    Promise.all([
+        knex.schema.table('CONDITIONS', table => {
+            table.dropIndex('', 'CONDITION_BY_TYPE');
+        }),
+        knex.raw(`CREATE INDEX CONDITION_BY_TYPE ON CONDITIONS (TYPE) INCLUDE (ID, TIMESTAMP, TEXT)
+                WITH (ONLINE = ON);`)
+    ]);
