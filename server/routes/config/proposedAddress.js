f9d7b1db10c716aea583c24251fe290e1e8bd0c2
mattwhitfield <whitfield.mj@gmail.com>
2018-01-30 13:38:34 +0000

    updated ids for feature tests. Implemented path decision for bassReferral

--- server/routes/config/proposedAddress.js
+++ server/routes/config/proposedAddress.js
@@ -11,12 +11,16 @@ module.exports = {
         }
     },
     bassReferral: {
-        nextPath: '/hdc/proposedAddress/curfewAddress/',
         fields: [
             {decision: {}},
             {proposedTown: {dependantOn: 'decision', predicate: 'Yes'}},
             {proposedCounty: {dependantOn: 'decision', predicate: 'Yes'}}
-        ]
+        ],
+        nextPathDecision: {
+            fieldToDecideOn: 'decision',
+            Yes: '/hdc/taskList/',
+            No: '/hdc/proposedAddress/curfewAddress/'
+        }
     },
     curfewAddress: {
         nextPath: '/hdc/taskList/',
