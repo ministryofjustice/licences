4827f6f551da799069c3acc1cd49301396c3d09f
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-02-07 13:19:13 +0000

    Delius staff IDs seed to match stage nomis

--- seeds/a003-staff-ids.js
+++ seeds/a003-staff-ids.js
@@ -11,8 +11,38 @@ exports.seed = knex =>
                 {
                     NOMIS_ID: "RO_USER",
                     STAFF_ID: "DELIUS_ID",
-                    FIRST_NAME: 'FIRSTA',
-                    LAST_NAME: 'LASTA'
+                    FIRST_NAME: 'JESSY',
+                    LAST_NAME: 'SMITH'
+                },
+                {
+                    NOMIS_ID: "RO_USER2",
+                    STAFF_ID: "DELIUS_USER2",
+                    FIRST_NAME: 'SHEILA',
+                    LAST_NAME: 'HANCOCK'
+                },
+                {
+                    NOMIS_ID: "RO_USER3",
+                    STAFF_ID: "DELIUS_USER3",
+                    FIRST_NAME: 'TREVOR',
+                    LAST_NAME: 'SMITH'
+                },
+                {
+                    NOMIS_ID: "RO_USER4",
+                    STAFF_ID: "DELIUS_USER4",
+                    FIRST_NAME: 'DAVID',
+                    LAST_NAME: 'BALL'
+                },
+                {
+                    NOMIS_ID: "RO_USER5",
+                    STAFF_ID: "DELIUS_USER5",
+                    FIRST_NAME: 'JULIE',
+                    LAST_NAME: 'WOOD'
+                },
+                {
+                    NOMIS_ID: "RO_USER6",
+                    STAFF_ID: "DELIUS_USER6",
+                    FIRST_NAME: 'LYDIA',
+                    LAST_NAME: 'HUME'
                 }
             ])
         );
