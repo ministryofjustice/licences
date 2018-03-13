2b3e860a110037f48f72311855bdf5d0e2d9fd13
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-03-13 15:54:33 +0000

    Make address views consistent

--- assets/javascripts/otherResidentsClient.js
+++ assets/javascripts/otherResidentsClient.js
@@ -13,7 +13,7 @@ function addResidentFields() {
     var numberRegEx = /\d+/g;
 
     var group = $(this).closest('.otherResidentsInput');
-    var finalResident = group.find('.otherResident:last');
+    var finalResident = group.find('.resident:last');
 
     var newIndex = parseInt(finalResident.find('input.residentName').attr('name').match(numberRegEx), 10) + 1;
     var newName = finalResident.find('input.residentName').attr('name').replace(numberRegEx, newIndex);
