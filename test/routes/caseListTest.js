bd16429c1ae02afea5c87c8c11dd17d2074be87e
mattwhitfield <whitfield.mj@gmail.com>
2018-03-21 10:48:32 +0000

    Added tabs for CA user

--- test/routes/caseListTest.js
+++ test/routes/caseListTest.js
@@ -7,6 +7,8 @@ const {
     authenticationMiddleware
 } = require('../supertestSetup');
 
+const getCaseListResponse = require('./stubs/caseListResponseCA');
+
 const createCaseListRoute = require('../../server/routes/caseList');
 
 const app = appSetup(createCaseListRoute({
@@ -17,9 +19,34 @@ const app = appSetup(createCaseListRoute({
 
 describe('GET /caseList', () => {
 
-    it('renders and HTML output', () => {
+    beforeEach(() => {
+        caseListServiceStub.getHdcCaseList.resolves(getCaseListResponse);
+    });
+
+    it('redirects to tab if none supplied', () => {
         return request(app)
             .get('/')
+            .expect(302)
+            .expect(res => {
+                expect(res.header.location).to.include('/caseList/ready');
+            });
+
+    });
+
+    it('redirects to tab if unexpected tab supplied', () => {
+        return request(app)
+            .get('/fefe')
+            .expect(302)
+            .expect(res => {
+                expect(res.header.location).to.include('/caseList/ready');
+            });
+
+    });
+
+
+    it('renders and HTML output', () => {
+        return request(app)
+            .get('/ready')
             .expect(200)
             .expect('Content-Type', /html/);
 
@@ -27,11 +54,89 @@ describe('GET /caseList', () => {
 
     it('renders the hdc eligible prisoners page', () => {
         return request(app)
-            .get('/')
+            .get('/ready')
             .expect(200)
             .expect(res => {
                 expect(res.text).to.include('id="hdcEligiblePrisoners">');
             });
 
+    });
+
+    context('user is CA', () => {
+        context('tab is ready', () => {
+            it('should filter out offenders at a stage that isnt UNSTARTED or ELIGIBILTY', () => {
+                return request(app)
+                    .get('/ready')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.text).to.not.include('Andrews, RO Processing');
+                        expect(res.text).to.include('Andrews, Unstarted');
+                        expect(res.text).to.include('Andrews, Eligibility');
+                        expect(res.text).to.not.include('Andrews, Approval');
+                        expect(res.text).to.not.include('Andrews, Approved');
+                        expect(res.text).to.not.include('Andrews, Refused');
+                    });
+            });
+        });
+        context('tab is submitterRo', () => {
+            it('should filter out offenders at a stage that isnt PROCESSING_RO', () => {
+                return request(app)
+                    .get('/submittedRo')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.text).to.include('Andrews, RO Processing');
+                        expect(res.text).to.not.include('Andrews, Unstarted');
+                        expect(res.text).to.not.include('Andrews, Eligibility');
+                        expect(res.text).to.not.include('Andrews, Approval');
+                        expect(res.text).to.not.include('Andrews, Approved');
+                        expect(res.text).to.not.include('Andrews, Refused');
+                    });
+            });
+        });
+        context('tab is submitterDm', () => {
+            it('should filter out offenders at a stage that isnt APPROVAL', () => {
+                return request(app)
+                    .get('/submittedDm')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.text).to.not.include('Andrews, RO Processing');
+                        expect(res.text).to.not.include('Andrews, Unstarted');
+                        expect(res.text).to.not.include('Andrews, Eligibility');
+                        expect(res.text).to.include('Andrews, Approval');
+                        expect(res.text).to.not.include('Andrews, Approved');
+                        expect(res.text).to.not.include('Andrews, Refused');
+                    });
+            });
+        });
+        context('tab is approved', () => {
+            it('should filter out offenders at a stage that isnt DECIDED with status of Approved', () => {
+                return request(app)
+                    .get('/approved')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.text).to.not.include('Andrews, RO Processing');
+                        expect(res.text).to.not.include('Andrews, Unstarted');
+                        expect(res.text).to.not.include('Andrews, Eligibility');
+                        expect(res.text).to.not.include('Andrews, Approval');
+                        expect(res.text).to.include('Andrews, Approved');
+                        expect(res.text).to.not.include('Andrews, Refused');
+                    });
+            });
+        });
+        context('tab is refused', () => {
+            it('should filter out offenders at a stage that isnt DECIDED with status of Refused', () => {
+                return request(app)
+                    .get('/refused')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.text).to.not.include('Andrews, RO Processing');
+                        expect(res.text).to.not.include('Andrews, Unstarted');
+                        expect(res.text).to.not.include('Andrews, Eligibility');
+                        expect(res.text).to.not.include('Andrews, Approval');
+                        expect(res.text).to.not.include('Andrews, Approved');
+                        expect(res.text).to.include('Andrews, Refused');
+                    });
+            });
+        });
     });
 });
