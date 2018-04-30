e58c1e7153ffa76ee2d0838c872a2b05e88cb535
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-04-30 14:56:18 +0100

    Postgres DB queries

--- server/data/dataAccess/db.js
+++ server/data/dataAccess/db.js
@@ -1,52 +1,21 @@
-const logger = require('../../../log.js');
+const logger = require('../../../log');
+const config = require('../../config');
+
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    user: config.db.username,
+    host: config.db.server,
+    database: 'licences',
+    password: config.db.password,
+    port: 5432
+});
+
+pool.on('error', (error, client) => {
+    logger.error('Unexpected error on idle client', error);
+});
 
 module.exports = {
-    disconnect,
-    connect,
-    addParams
+    query: (text, params) => pool.query(text, params)
 };
 
-function connect() {
-    const config = require('../../config');
-    const Connection = require('tedious').Connection;
-
-    let connection = new Connection({
-        userName: config.db.username,
-        password: config.db.password,
-        server: config.db.server,
-        options: {
-            encrypt: true,
-            database: config.db.database,
-            useColumnNames: true,
-            rowCollectionOnRequestCompletion: true
-        }
-    });
-
-    connection.on('error', function(err) {
-        if(err.message === 'Connection lost - read ECONNRESET') {
-            logger.warn('Connection lost - read ECONNRESET');
-            logger.info('Azure loadbalancer timeout error - see https://github.com/tediousjs/tedious/issues/300');
-        } else {
-            logger.error('DB error: ' + err.message);
-        }
-    });
-
-    logger.debug('Created new DB connection');
-    return connection;
-}
-
-function addParams(params, request) {
-    params.forEach(function(param) {
-        let paramValue = param.value;
-
-        request.addParameter(
-            param.column,
-            param.type,
-            paramValue);
-    });
-}
-
-function disconnect(connection) {
-    logger.debug('Closing DB connection on disconnect');
-    connection.close();
-}
