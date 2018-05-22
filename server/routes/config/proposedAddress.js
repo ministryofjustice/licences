74b3e9a3ba0b36b4f27189baeeaf3f8c4a08588f
mattwhitfield <whitfield.mj@gmail.com>
2018-05-22 09:06:04 +0100

    Added in place validation to address proposed question

--- server/routes/config/proposedAddress.js
+++ server/routes/config/proposedAddress.js
@@ -19,6 +19,7 @@ module.exports = {
         fields: [
             {decision: {}}
         ],
+        validateInPlace: true,
         nextPath: {
             decisions: {
                 discriminator: 'decision',
