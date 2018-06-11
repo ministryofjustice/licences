b39771505ba510b82e30d70672e98acfff18bc35
mattwhitfield <whitfield.mj@gmail.com>
2018-06-11 15:38:00 +0100

    added middleware to refresh cookie

--- server/app.js
+++ server/app.js
@@ -117,6 +117,13 @@ module.exports = function createApp({
         next();
     });
 
+    // Update a value in the cookie so that the set-cookie will be sent.
+    // Only changes every minute so that it's not sent with every request.
+    app.use(function(req, res, next) {
+        req.session.nowInMinutes = Math.floor(Date.now() / 60e3);
+        next();
+    });
+
     // Resource Delivery Configuration
     app.use(compression());
 
