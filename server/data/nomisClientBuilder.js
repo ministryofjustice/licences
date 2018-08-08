6da0e7bfbb0b12817ed54a2f67d0fc499206466f
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-08-08 09:42:31 +0100

    Remove unused

--- server/data/nomisClientBuilder.js
+++ server/data/nomisClientBuilder.js
@@ -107,8 +107,6 @@ function nomisGetBuilder(token) {
             throw new NoTokenError();
         }
 
-        logger.debug(`Calling nomis with token: ${token}`);
-
         try {
             const result = await superagent
                 .get(path)
