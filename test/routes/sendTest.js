885475c24f043af6d15fdf96a589011f2474b0f8
mattwhitfield <whitfield.mj@gmail.com>
2018-08-20 11:53:27 +0100

    secured the send urls to appropriate roles

--- test/routes/sendTest.js
+++ test/routes/sendTest.js
@@ -30,9 +30,9 @@ describe('send', () => {
     describe('Get send/:destination/:nomisId', () => {
 
         it('renders caToRo form when addressReview is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, caUser);
             return request(app)
-                .get('/addressReview/123')
+                .get('/hdc/send/addressReview/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -41,9 +41,9 @@ describe('send', () => {
         });
 
         it('renders roToCa form when finalChecks is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, roUser);
             return request(app)
-                .get('/finalChecks/123')
+                .get('/hdc/send/finalChecks/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -52,9 +52,9 @@ describe('send', () => {
         });
 
         it('renders caToDm form when approval is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, caUser);
             return request(app)
-                .get('/approval/123')
+                .get('/hdc/send/approval/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -63,9 +63,9 @@ describe('send', () => {
         });
 
         it('renders dmToCa form when decided is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, dmUser);
             return request(app)
-                .get('/decided/123')
+                .get('/hdc/send/decided/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -74,9 +74,9 @@ describe('send', () => {
         });
 
         it('renders caToDmRefusal form when refusal is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, caUser);
             return request(app)
-                .get('/refusal/123')
+                .get('/hdc/send/refusal/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -85,9 +85,9 @@ describe('send', () => {
         });
 
         it('renders dmToCaReturn form when return is destination', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, dmUser);
             return request(app)
-                .get('/return/123')
+                .get('/hdc/send/return/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -96,9 +96,9 @@ describe('send', () => {
         });
 
         it('gets a submission target for caToRo', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, caUser);
             return request(app)
-                .get('/addressReview/123')
+                .get('/hdc/send/addressReview/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
@@ -107,23 +107,33 @@ describe('send', () => {
         });
 
         it('gets a submission target for roToCa', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, roUser);
             return request(app)
-                .get('/finalChecks/123')
+                .get('/hdc/send/finalChecks/123')
                 .expect(200)
                 .expect('Content-Type', /html/)
                 .expect(res => {
                     expect(res.text).to.contain('name="submissionTarget" value="HMP Blah"');
                 });
         });
+
+        it('should throw if get requested by wrong user', () => {
+
+            const app = createApp({licenceService, prisonerService}, roUser);
+            return request(app)
+                .get('/hdc/send/refusal/123')
+                .expect(403);
+
+        });
     });
 
-    describe('POST /send', () => {
-        it('calls markForHandover via licenceService', () => {
+    describe('POST send/:destination/:nomisId', () => {
+
+        it('calls markForHandover via licenceService for addressReview', () => {
             const app = createApp({licenceService, prisonerService});
 
             return request(app)
-                .post('/')
+                .post('/hdc/send/addressReview/123')
                 .send({nomisId: 123, transitionType: 'caToRo'})
                 .expect(() => {
                     expect(licenceService.markForHandover).to.be.calledOnce();
@@ -131,14 +141,84 @@ describe('send', () => {
                         123, {licence: {key: 'value'}}, 'caToRo'
                     );
                 });
-
         });
 
-        it('audits the send event', () => {
-            const app = createApp({licenceService, prisonerService});
+        it('calls markForHandover via licenceService for finalChecks', () => {
+            const app = createApp({licenceService, prisonerService}, roUser);
 
             return request(app)
-                .post('/')
+                .post('/hdc/send/finalChecks/123')
+                .send({nomisId: 123, transitionType: 'roToCa'})
+                .expect(() => {
+                    expect(licenceService.markForHandover).to.be.calledOnce();
+                    expect(licenceService.markForHandover).to.be.calledWith(
+                        123, {licence: {key: 'value'}}, 'roToCa'
+                    );
+                });
+        });
+
+        it('calls markForHandover via licenceService for approval', () => {
+            const app = createApp({licenceService, prisonerService}, caUser);
+
+            return request(app)
+                .post('/hdc/send/approval/123')
+                .send({nomisId: 123, transitionType: 'caToDm'})
+                .expect(() => {
+                    expect(licenceService.markForHandover).to.be.calledOnce();
+                    expect(licenceService.markForHandover).to.be.calledWith(
+                        123, {licence: {key: 'value'}}, 'caToDm'
+                    );
+                });
+        });
+
+        it('calls markForHandover via licenceService for decided', () => {
+            const app = createApp({licenceService, prisonerService}, dmUser);
+
+            return request(app)
+                .post('/hdc/send/decided/123')
+                .send({nomisId: 123, transitionType: 'dmToCa'})
+                .expect(() => {
+                    expect(licenceService.markForHandover).to.be.calledOnce();
+                    expect(licenceService.markForHandover).to.be.calledWith(
+                        123, {licence: {key: 'value'}}, 'dmToCa'
+                    );
+                });
+        });
+
+        it('calls markForHandover via licenceService for refusal', () => {
+            const app = createApp({licenceService, prisonerService}, caUser);
+
+            return request(app)
+                .post('/hdc/send/refusal/123')
+                .send({nomisId: 123, transitionType: 'caToDmRefusal'})
+                .expect(() => {
+                    expect(licenceService.markForHandover).to.be.calledOnce();
+                    expect(licenceService.markForHandover).to.be.calledWith(
+                        123, {licence: {key: 'value'}}, 'caToDmRefusal'
+                    );
+                });
+        });
+
+        it('calls markForHandover via licenceService for return', () => {
+            const app = createApp({licenceService, prisonerService}, dmUser);
+
+            return request(app)
+                .post('/hdc/send/return/123')
+                .send({nomisId: 123, transitionType: 'dmToCaReturn'})
+                .expect(() => {
+                    expect(licenceService.markForHandover).to.be.calledOnce();
+                    expect(licenceService.markForHandover).to.be.calledWith(
+                        123, {licence: {key: 'value'}}, 'dmToCaReturn'
+                    );
+                });
+        });
+
+
+        it('audits the send event', () => {
+            const app = createApp({licenceService, prisonerService}, dmUser);
+
+            return request(app)
+                .post('/hdc/send/return/123')
                 .send({
                     nomisId: 123,
                     sender: 'from',
@@ -159,15 +239,26 @@ describe('send', () => {
         });
 
         it('shows sent confirmation', () => {
-            const app = createApp({licenceService, prisonerService});
+            const app = createApp({licenceService, prisonerService}, dmUser);
 
             return request(app)
-                .post('/')
+                .post('/hdc/send/return/123')
                 .send({nomisId: 123, sender: 'from', receiver: 'to', transitionType: 'foobar'})
                 .expect(302)
                 .expect(res => {
                     expect(res.header['location']).to.eql('/hdc/sent/foobar');
                 });
+
+        });
+
+        it('should throw if post requested by wrong user', () => {
+
+            const app = createApp({licenceService, prisonerService}, caUser);
+
+            return request(app)
+                .post('/hdc/send/return/123')
+                .send({nomisId: 123, sender: 'from', receiver: 'to', transitionType: 'foobar'})
+                .expect(403);
 
         });
     });
@@ -180,6 +271,20 @@ const caUser = {
     role: 'CA'
 };
 
+const roUser = {
+    staffId: 'my-staff-id',
+    token: 'my-token',
+    email: 'user@email',
+    role: 'RO'
+};
+
+const dmUser = {
+    staffId: 'my-staff-id',
+    token: 'my-token',
+    email: 'user@email',
+    role: 'DM'
+};
+
 function createApp({licenceService, prisonerService}, user = caUser) {
     return appSetup(createSendRoute({
         licenceService,
@@ -187,5 +292,5 @@ function createApp({licenceService, prisonerService}, user = caUser) {
         logger: loggerStub,
         authenticationMiddleware,
         audit: auditStub
-    }), user);
+    }), user, '/hdc/send/');
 }
