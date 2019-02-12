0addc09f84eda5dd54d751efae015a76df08f36c
mattwhitfield <whitfield.mj@gmail.com>
2019-02-12 11:39:04 +0000

    moved standard ro tasks into separate file

--- server/routes/viewModels/taskLists/roTasks.js
+++ server/routes/viewModels/taskLists/roTasks.js
@@ -2,8 +2,80 @@ const riskManagement = require('./tasks/riskManagement');
 const curfewHours = require('./tasks/curfewHours');
 const additionalConditions = require('./tasks/additionalConditions');
 const reportingInstructions = require('./tasks/reportingInstructions');
+const curfewAddress = require('./tasks/curfewAddress');
+const victimLiaison = require('./tasks/victimLiaison');
 
 module.exports = {
+    getRoTasks: ({decisions}) => {
+        const {
+            bassReferralNeeded,
+            addressUnsuitable,
+            curfewAddressRejected,
+            addressReviewFailed
+        } = decisions;
+
+        const addressRejectedInRiskPhase = curfewAddressRejected && addressUnsuitable;
+        const addressRejectedInReviewPhase = curfewAddressRejected && addressReviewFailed;
+
+        return [
+            {task: 'bassAreaTask'},
+            {
+                title: 'Proposed curfew address',
+                label: curfewAddress.getLabel,
+                action: curfewAddress.getRoAction
+            },
+            {
+                title: 'Risk management',
+                label: riskManagement.getLabel,
+                action: riskManagement.getRoAction
+            },
+            {
+                title: 'Victim liaison',
+                label: victimLiaison.getLabel,
+                action: victimLiaison.getRoAction
+            },
+            {
+                title: 'Curfew hours',
+                label: curfewHours.getLabel,
+                action: curfewHours.getRoAction
+            },
+            {
+                title: 'Additional conditions',
+                label: additionalConditions.getLabel,
+                action: additionalConditions.getRoAction
+            },
+            {
+                title: 'Reporting instructions',
+                label: reportingInstructions.getLabel,
+                action: reportingInstructions.getRoAction
+            },
+            {task: 'roSubmitTask', filters: []}
+        ].filter(task => {
+            if (task.task === 'bassAreaTask') {
+                return bassReferralNeeded;
+            }
+
+            if (task.title === 'Proposed curfew address') {
+                return !bassReferralNeeded && !curfewAddressRejected || addressRejectedInReviewPhase;
+            }
+
+            if (task.title === 'Risk management') {
+                return !curfewAddressRejected || addressRejectedInRiskPhase;
+            }
+
+            if ([
+                'Victim liaison',
+                'Curfew hours',
+                'Additional conditions',
+                'Reporting instructions'
+            ].includes(task.title)) {
+                return !curfewAddressRejected;
+            }
+
+            return true;
+        });
+    },
+
     getRoTasksPostApproval: () => {
         return [
             {
