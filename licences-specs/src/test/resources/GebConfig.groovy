33a9e8c61a18594a1bf6a6fc61e72c1b3ce4dac3
Peter Phillips <peter@greenthistle.co.uk>
2019-02-18 12:01:38 +0000

    LIC-881: Bump timeout for test failures

--- licences-specs/src/test/resources/GebConfig.groovy
+++ licences-specs/src/test/resources/GebConfig.groovy
@@ -1,6 +1,12 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 
+atCheckWaiting = true
+
+waiting {
+    timeout = 2
+}
+
 environments {
     chrome {
         driver = { new ChromeDriver() }
