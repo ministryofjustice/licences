77f1791c748a7e059b010a39a44fd361eed223f7
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-03-21 09:09:01 +0000

    LT4 stage test data mailbox config

--- server/services/config/notificationMailboxes.js
+++ server/services/config/notificationMailboxes.js
@@ -6,6 +6,10 @@ module.exports = {
       { email: 'hdc_test+2@digital.justice.gov.uk', name: 'DM Test2' },
     ],
   },
+  LT4: {
+    ca: ['hdc_test@digital.justice.gov.uk'],
+    dm: [{ email: 'hdc_test@digital.justice.gov.uk', name: 'DM StageTest' }],
+  },
   DTI: {
     ca: ['OMU.Deerbolt@hmps.gsi.gov.uk'],
     dm: [{ email: 'DM.Deerbolt@hmps.gsi.gov.uk', name: 'Deerbolt Decision Maker' }],
