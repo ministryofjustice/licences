e8d264ed351936a29c005b9ee061f508d223591d
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-05-23 10:32:22 +0100

    Fix function order

--- server/services/jobs/jobUtils.js
+++ server/services/jobs/jobUtils.js
@@ -7,11 +7,36 @@ module.exports = {
   onceOnly,
 }
 
-async function queryForLock(name, lockFunction) {
-  const identifier = hash(name)
-  const query = { text: `select ${lockFunction}($1)`, values: [identifier] }
-  const res = await db.query(query)
-  return getIn(res, ['rows', [0], lockFunction])
+function onceOnly(jobFunction, name, overlapTimeout, callback) {
+  return async () => {
+    try {
+      const result = await withLock(jobFunction, name, overlapTimeout)
+      callback(null, result)
+    } catch (error) {
+      callback(error, null)
+    }
+  }
+}
+
+async function withLock(jobFunction, name, overlapTimeout) {
+  try {
+    logger.info(`Trying lock for: ${name}`)
+    const locked = await tryLock(name)
+    if (locked) {
+      logger.info(`${name}: Obtained lock`)
+      const result = await jobFunction()
+      await delay(overlapTimeout)
+      return result
+    }
+
+    logger.info(`${name}: Could not obtain lock`)
+    return 'MISSED_LOCK'
+  } catch (error) {
+    logger.error(`Error running job: ${name}:`, error)
+    throw error
+  } finally {
+    await unlock(name)
+  }
 }
 
 async function tryLock(name) {
@@ -22,50 +47,15 @@ async function unlock(name) {
   return queryForLock(name, 'pg_advisory_unlock')
 }
 
-async function withLock(jobFunction, name, overlapTimeout) {
-  try {
-    logger.info(`Trying lock for: ${name}`)
-    const locked = await tryLock(name)
-    if (locked) {
-      logger.info(`${name}: Obtained lock`)
-      return runJob(jobFunction, name, overlapTimeout)
-    }
-
-    logger.info(`${name}: Could not obtain lock`)
-    return 'MISSED_LOCK'
-  } catch (error) {
-    logger.error(`${name}: Error obtaining lock`, error)
-    throw error
-  } finally {
-    await unlock(name)
-  }
+async function queryForLock(name, lockFunction) {
+  const identifier = hash(name)
+  const query = { text: `select ${lockFunction}($1)`, values: [identifier] }
+  const res = await db.query(query)
+  return getIn(res, ['rows', [0], lockFunction])
 }
 
 async function delay(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms)
   })
-}
-
-async function runJob(jobFunction, name, overlapTimeout) {
-  try {
-    logger.info(`Running job: ${name}`)
-    const result = await jobFunction()
-    await delay(overlapTimeout)
-    return result
-  } catch (error) {
-    logger.error(`Error running job: ${name}:`, error)
-    throw error
-  }
-}
-
-function onceOnly(jobFunction, name, overlapTimeout, callback) {
-  return async () => {
-    try {
-      const result = await withLock(jobFunction, name, overlapTimeout)
-      callback(null, result)
-    } catch (error) {
-      callback(error, null)
-    }
-  }
 }
