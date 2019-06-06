f15d554e85cb41177915edda63ee3488ed346ce0
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-06-06 11:28:11 +0100

    Add Role Name for app insights

--- azure-appinsights.js
+++ azure-appinsights.js
@@ -1,11 +1,14 @@
 const appInsights = require('applicationinsights')
+const fs = require('fs')
 require('dotenv').config()
 
+const packageData = JSON.parse(fs.readFileSync('./package.json'))
 if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   appInsights
     .setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
     .setAutoCollectExceptions(false) // logger handles these
     .start()
+  appInsights.defaultClient.context.tags['ai.cloud.role'] = `${packageData.name}`
   module.exports = appInsights
 } else {
   module.exports = null
