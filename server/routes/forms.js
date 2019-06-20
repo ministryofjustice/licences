7796e86952d0b15bbbcfc94ac049ed8c03b03947
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-06-20 15:39:54 +0100

    Generate curfew address check form using express-pug-pdf

--- server/routes/forms.js
+++ server/routes/forms.js
@@ -1,8 +1,44 @@
+const moment = require('moment')
 const { asyncMiddleware } = require('../utils/middleware')
-const { formTemplates } = require('../config')
+const { formTemplates, formsDateFormat, pdfOptions, domain } = require('../config')
 const { isEmpty } = require('../utils/functionalHelpers')
 
-module.exports = ({ formService }) => router => {
+module.exports = ({ formService, conditionsService, prisonerService, configClient }) => router => {
+  router.get(
+    '/curfewAddress/:bookingId',
+    asyncMiddleware(async (req, res) => {
+      const { bookingId } = req.params
+      const {
+        prisoner,
+        licence: { licence },
+        licenceStatus,
+      } = res.locals
+
+      const [fullLicence, standardConditions, fullPrisoner, ro, ca] = await Promise.all([
+        conditionsService.populateLicenceWithApprovedConditions(licence),
+        conditionsService.getStandardConditions(),
+        prisonerService.getPrisonerDetails(bookingId, req.user.token),
+        prisonerService.getResponsibleOfficer(bookingId, req.user.token),
+        configClient.getMailboxes(prisoner.agencyLocationId, 'CA'),
+      ])
+
+      return res.renderPDF(
+        'forms/curfewAddress',
+        {
+          prisoner: fullPrisoner,
+          licence: fullLicence,
+          standardConditions,
+          isBass: licenceStatus.decisions.bassReferralNeeded,
+          ro,
+          ca: ca[0] || {},
+          domain,
+          completionDate: moment().format(formsDateFormat),
+        },
+        { filename: `${fullPrisoner.offenderNo}.pdf`, pdfOptions }
+      )
+    })
+  )
+
   router.get(
     '/:templateName/:bookingId',
     asyncMiddleware(async (req, res) => {
