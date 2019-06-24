eb405a16cf711da56ff19fb394db8d33d7639f9b
bell-pa <peter.bell@digital.justice.gov.uk>
2019-06-24 13:30:10 +0100

    LIC-1000 Revert GebConfig.groovy

--- licences-specs/src/test/resources/GebConfig.groovy
+++ licences-specs/src/test/resources/GebConfig.groovy
@@ -23,10 +23,10 @@ environments {
 
 // Default if geb.env is not set to one of 'chrome', or 'chromeHeadless'
 driver = {
-  // Uncomment for mac
-   System.setProperty('webdriver.chrome.driver', '/usr/local/bin/chromedriver')
+  System.setProperty('webdriver.chrome.driver', '/Applications/chromedriver')
+//  System.setProperty('webdriver.chrome.driver', '/usr/local/bin/chromedriver')
   ChromeOptions options = new ChromeOptions()
-  options.addArguments('chrome')
+  options.addArguments('headless')
   new ChromeDriver(options)
 }
 
