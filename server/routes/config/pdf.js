21108e9be50207fc16661248160cea0f19aa6ac9
atodd-noms <alistair.todd@digital.justice.gov.uk>
2019-06-27 13:20:48 +0100

    Move HDC-AP licence out of PDF service

--- server/routes/config/pdf.js
+++ server/routes/config/pdf.js
@@ -3,22 +3,27 @@ module.exports = {
     {
       id: 'hdc_ap_pss',
       label: 'AP PSS HDC Licence',
+      version: '1.0',
     },
     {
       id: 'hdc_yn',
       label: "HDC Young Person's Licence",
+      version: '1.0',
     },
     {
       id: 'hdc_ap',
       label: 'AP HDC Licence',
+      version: '1.0',
     },
     {
       id: 'hdc_pss',
       label: 'HDC PSS Notice of Supervision',
+      version: '1.0',
     },
     {
       id: 'hdc_u12',
       label: 'HDC Under 12 month licence',
+      version: '1.0',
     },
   ],
 }
