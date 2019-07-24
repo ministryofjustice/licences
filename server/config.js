ce697bbf2e3fa4f236f0ea2b253a0571a21b6c8c
bell-pa <peter.bell@digital.justice.gov.uk>
2019-07-24 11:11:26 +0100

    LIC-1032 Migrate 'vary' templates. LIC-939 Allow for start date for licence vary. (#953)
    * LIC-1031 Migrate remaining standard licence templates.
    
    * LIC-1031 Migrate remaining standard licence templates.

--- server/config.js
+++ server/config.js
@@ -63,7 +63,18 @@ module.exports = {
     licences: {
       pdfServiceHost: get('PDF_SERVICE_HOST', 'http://localhost:8081'),
       taggingCompanyTelephone: get('TAGGING_CO_PHONE', '01234 567890'),
-      localTemplates: ['hdc_ap', 'vary_hdc_ap', 'hdc_ap_pss', 'hdc_pss', 'hdc_u12', 'hdc_yn'],
+      localTemplates: [
+        'hdc_ap',
+        'vary_hdc_ap',
+        'hdc_ap_pss',
+        'vary_hdc_ap_pss',
+        'hdc_pss',
+        'vary_hdc_pss',
+        'hdc_u12',
+        'vary_hdc_u12',
+        'hdc_yn',
+        'vary_hdc_yn',
+      ],
       pdfOptions: {
         format: 'A4',
         margin: {
