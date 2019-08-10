281f88fef9ed5c0853cfa732158693c09957b740
bell-pa <peter.bell@digital.justice.gov.uk>
2019-08-10 17:02:49 +0100

    LIC-1063 Send e-mail notification direct to the Responsible Officer. LIC-1059 Use new email templates for messages copied to clearing office.

--- server/services/config/notificationTemplates.js
+++ server/services/config/notificationTemplates.js
@@ -2,10 +2,7 @@ module.exports = {
   CA_RETURN: {
     templateId: '7e581130-de71-4b3f-bac0-ce95c0275525',
   },
-  DM_TO_CA_RETURN: {
-    templateId: '7e581130-de71-4b3f-bac0-ce95c0275525',
-  },
-  CA_RETURN_CO_COPY: {
+  CA_RETURN_ADMIN: {
     templateId: '85c1151b-8e85-431b-b8b3-bae3c4409198',
   },
   CA_DECISION: {
@@ -14,7 +11,7 @@ module.exports = {
   RO_NEW: {
     templateId: '9afc4ab7-5d3f-4703-805b-8d5b7c0c7cea',
   },
-  RO_NEW_CO_COPY: {
+  RO_NEW_ADMIN: {
     templateId: 'f93bc690-c8a6-4c7f-8a19-4d075cea4c3e',
   },
   RO_TWO_DAYS: {
@@ -26,7 +23,7 @@ module.exports = {
   RO_OVERDUE: {
     templateId: '69beb252-f4b1-4837-9715-cc429e448664',
   },
-  RO_OVERDUE_CO_COPY: {
+  RO_OVERDUE_ADMIN: {
     templateId: '749d1833-96e5-4bcf-b6a8-befb6c00ed08',
   },
   DM_NEW: {
