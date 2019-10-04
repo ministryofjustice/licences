479beaadba3dc7bd32e43801e86bae4017fcb231
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-10-04 15:09:26 +0100

    DCS-183 Adding the ability to override delius client id and secret

--- server/authentication/oauth.js
+++ server/authentication/oauth.js
@@ -1,9 +1,7 @@
-const querystring = require('querystring')
 const config = require('../config')
 
 const generate = (clientId, clientSecret) => {
-  const token = Buffer.from(`${querystring.escape(clientId)}:${querystring.escape(clientSecret)}`).toString('base64')
-
+  const token = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
   return `Basic ${token}`
 }
 
