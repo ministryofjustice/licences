aa24374a1f90c923161ad2ff597d55c76da35852
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2019-12-06 11:04:28 +0000

    DCS-263-add UNIQUE constraint (#1028)
    The migration to create the active_local_delivery_units table has a column called ldu_code.
    The UNIQUE constraint was previously missed so a new migration has been created to alter the table and add the constraint to ldu_code

---
+++ migrations/20191206100315_alter_active_local_delivery_units.js
@@ -0,0 +1,20 @@
+exports.up = knex =>
+  Promise.all([
+    knex.schema.alterTable('active_local_delivery_units', table => {
+      table
+        .string('ldu_code', 10)
+        .notNullable()
+        .unique()
+        .alter()
+    }),
+  ])
+
+exports.down = knex =>
+  Promise.all([
+    knex.schema.alterTable('active_local_delivery_units', table => {
+      table
+        .string('ldu_code', 10)
+        .notNullable()
+        .alter()
+    }),
+  ])
