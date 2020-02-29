8fe58de1c688887dd68111fee0b6354b3c01f9b7
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2020-02-29 08:04:36 +0000

    DCS-364 Filtering out dodgy probation areas  (#1114)
    * DCS-364 Filtering probation areas to only show active and to ignore prisons
    
    * DCS-364 Redirect to root after changing role

--- server/routes/user.js
+++ server/routes/user.js
@@ -29,7 +29,7 @@ module.exports = ({ userService }) => router => {
         await userService.setActiveCaseLoad(caseLoadId, req.user, res.locals.token)
       }
 
-      res.redirect('/user')
+      res.redirect('/')
     })
   )
 
