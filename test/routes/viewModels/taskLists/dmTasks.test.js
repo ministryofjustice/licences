7f6bef94013edbf86d3a5f1c25fab972e003253a
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-03-03 11:03:12 +0000

    DCS-256 Fix bug where DM incorrectly sees rejected address task list
    The Decision manager was presented the rejected list when an address is rejected and an offender goes on to have a BASS address accepted.
    This is because we don't track decisions associated with an address and BASS and normal addresses are just stored as different parts of the JSON payload.
    We can't prioritise decisons made associated with one type of address over the other.
    
    This tweaks how we determine which task list to present.
    
    This is merely the whacking one mole... we have some ideas of how to fix the root cause but this is just a sticking plaster.

---
+++ test/routes/viewModels/taskLists/dmTasks.test.js
@@ -0,0 +1,109 @@
+const dmTasks = require('../../../../server/routes/viewModels/taskLists/dmTasks.js')
+
+describe('dmTasks', () => {
+  const tasks = {}
+
+  describe('rejected task list', () => {
+    test('withdrawn address shows rejected list', () => {
+      const results = dmTasks({ tasks, decisions: { addressWithdrawn: true, bassAccepted: 'No' } })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'eligibilitySummaryTask',
+        'Proposed curfew address',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+
+    test('rejected curfew address shows rejected list with risk management', () => {
+      const results = dmTasks({ tasks, decisions: { curfewAddressRejected: true } })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'eligibilitySummaryTask',
+        'Proposed curfew address',
+        'Risk management',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+  })
+
+  describe('standard task list', () => {
+    test('default list', () => {
+      const results = dmTasks({
+        tasks,
+        decisions: {},
+      })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'Proposed curfew address',
+        'Risk management',
+        'Victim liaison',
+        'Curfew hours',
+        'Additional conditions',
+        'Reporting instructions',
+        'Review case',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+
+    test('Accepted BASS address', () => {
+      const results = dmTasks({
+        tasks,
+        decisions: { bassReferralNeeded: true, bassAccepted: 'Yes' },
+      })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'BASS address',
+        'Risk management',
+        'Victim liaison',
+        'Curfew hours',
+        'Additional conditions',
+        'Reporting instructions',
+        'Review case',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+
+    test('Confiscation order', () => {
+      const results = dmTasks({
+        tasks,
+        decisions: { confiscationOrder: true },
+      })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'Proposed curfew address',
+        'Risk management',
+        'Victim liaison',
+        'Curfew hours',
+        'Additional conditions',
+        'Reporting instructions',
+        'Review case',
+        'Postpone',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+
+    test('previously rejected address with bass accepted', () => {
+      const results = dmTasks({
+        tasks,
+        decisions: { curfewAddressRejected: true, bassReferralNeeded: true, bassAccepted: 'Yes' },
+      })
+
+      expect(results.map(({ task, title }) => task || title)).toStrictEqual([
+        'BASS address',
+        'Risk management',
+        'Victim liaison',
+        'Curfew hours',
+        'Additional conditions',
+        'Reporting instructions',
+        'Review case',
+        'Return to prison case admin',
+        'Final decision',
+      ])
+    })
+  })
+})
