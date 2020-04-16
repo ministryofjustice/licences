20591a07593dd9162086661d1c3084099fb37dd4
bell-pa <peter.bell@digital.justice.gov.uk>
2020-04-16 10:21:40 +0100

    DCS-391 refactor taskListModels.js

--- server/routes/viewModels/taskListModels.js
+++ server/routes/viewModels/taskListModels.js
@@ -5,32 +5,6 @@ const { getAllowedTransition } = require('../../utils/licenceStatusTransitions')
 const { getCaTasksEligibility, getCaTasksFinalChecks, getCaTasksPostApproval } = require('./taskLists/caTasks')
 const getVaryTasks = require('./taskLists/varyTasks')
 
-const taskListsConfig = {
-  caTasksEligibility: {
-    stages: ['ELIGIBILITY', 'UNSTARTED'],
-    role: 'CA',
-  },
-  caTasksPostApproval: {
-    stages: ['DECIDED', 'MODIFIED', 'MODIFIED_APPROVAL'],
-    role: 'CA',
-  },
-  caTasksFinalChecks: {
-    stages: ['PROCESSING_CA', 'PROCESSING_RO', 'APPROVAL'],
-    role: 'CA',
-  },
-  roTasks: {
-    stages: ['PROCESSING_RO', 'PROCESSING_CA', 'APPROVAL', 'ELIGIBILITY'],
-    role: 'RO',
-  },
-  roTasksPostApproval: {
-    stages: ['DECIDED', 'MODIFIED', 'MODIFIED_APPROVAL'],
-    role: 'RO',
-  },
-  dmTasks: {
-    role: 'DM',
-  },
-}
-
 module.exports = (
   role,
   postRelease,
@@ -38,47 +12,57 @@ module.exports = (
   { version = null, versionDetails = null, approvedVersion = {}, approvedVersionDetails = {}, licence = {} } = {}
 ) => {
   const { decisions, tasks, stage } = licenceStatus
-  const taskListName = getTaskListName(role, stage, postRelease)
   const allowedTransition = getAllowedTransition(licenceStatus, role)
-  switch (taskListName) {
-    case 'dmTasks':
-      return getDmTasks({ decisions, tasks, stage })
 
-    case 'roTasks':
-      return getRoTasks({ decisions, tasks, allowedTransition })
-
-    case 'roTasksPostApproval':
-      return getRoTasksPostApproval({ decisions, tasks })
-
-    case 'caTasksEligibility':
-      return getCaTasksEligibility({ decisions, tasks, allowedTransition })
-
-    case 'caTasksFinalChecks':
-      return getCaTasksFinalChecks({ decisions, tasks, allowedTransition })
-
-    case 'caTasksPostApproval':
-      return getCaTasksPostApproval(stage)({ decisions, tasks, allowedTransition })
-
-    case 'vary':
-      return getVaryTasks({ version, versionDetails, approvedVersion, approvedVersionDetails, licence })({ stage })
-
-    default:
-      return [
-        {
-          title: 'No active licence',
-          action: { type: 'link', text: 'Return to case list', href: '/caseList/' },
-        },
-      ]
-  }
-}
-
-function getTaskListName(role, stage, postRelease) {
   if (postRelease) {
-    return 'vary'
+    return getVaryTasks({ version, versionDetails, approvedVersion, approvedVersionDetails, licence })({ stage })
   }
 
-  return (
-    pickKey((view) => view.role === role && (!view.stages || view.stages.includes(stage)))(taskListsConfig) ||
-    'noTaskList'
-  )
+  if (role === 'DM') {
+    return getDmTasks({ decisions, tasks, stage })
+  }
+
+  if (role === 'CA') {
+    switch (stage) {
+      case 'UNSTARTED':
+      case 'ELIGIBILITY':
+        return getCaTasksEligibility({ decisions, tasks, allowedTransition })
+
+      case 'PROCESSING_RO':
+      case 'PROCESSING_CA':
+      case 'APPROVAL':
+        return getCaTasksFinalChecks({ decisions, tasks, allowedTransition })
+
+      case 'DECIDED':
+      case 'MODIFIED':
+      case 'MODIFIED_APPROVAL':
+        return getCaTasksPostApproval(stage)({ decisions, tasks, allowedTransition })
+
+      default: // fall through
+    }
+  }
+
+  if (role === 'RO') {
+    switch (stage) {
+      case 'ELIGIBILITY':
+      case 'PROCESSING_RO':
+      case 'PROCESSING_CA':
+      case 'APPROVAL':
+        return getRoTasks({ decisions, tasks, allowedTransition })
+
+      case 'DECIDED':
+      case 'MODIFIED':
+      case 'MODIFIED_APPROVAL':
+        return getRoTasksPostApproval({ decisions, tasks })
+
+      default: // fall through
+    }
+  }
+
+  return [
+    {
+      title: 'No active licence',
+      action: { type: 'link', text: 'Return to case list', href: '/caseList/' },
+    },
+  ]
 }
