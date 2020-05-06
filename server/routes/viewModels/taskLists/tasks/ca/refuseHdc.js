441cd3ac65677080f20a4919aa3f1b5d88bca128
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-05-06 10:15:53 +0100

    DCS-476 Showing reason when CA refuses case before DM handover
    Also fixing refuse button - it previously never changed value even if refused
    This is because it was looking at the wrong decision
    
    Tidied up the logic for displaying warning message too.

--- server/routes/viewModels/taskLists/tasks/ca/refuseHdc.js
+++ server/routes/viewModels/taskLists/tasks/ca/refuseHdc.js
@@ -1,11 +1,11 @@
 const { taskBtn } = require('../utils/actions')
 
 module.exports = ({ decisions }) => {
-  const { refused } = decisions
+  const { caRefused } = decisions
   return {
     title: null,
-    label: refused ? 'HDC refused' : 'Refuse the case if there is no available address or not enough time',
-    action: refused
+    label: caRefused ? 'HDC refused' : 'Refuse the case if there is no available address or not enough time',
+    action: caRefused
       ? taskBtn('/hdc/finalChecks/refuse/', 'Update refusal', false, 'refuse')
       : taskBtn('/hdc/finalChecks/refuse/', 'Refuse HDC', true, 'refuse'),
   }
