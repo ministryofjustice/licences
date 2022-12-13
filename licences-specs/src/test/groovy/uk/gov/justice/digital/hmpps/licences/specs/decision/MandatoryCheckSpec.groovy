4b13820c58d56e47ed7aef53b975d08a96ddb9cf
David Thomas <david.thomas1@digital.justice.gov.uk>
2022-12-13 14:50:09 +0000

    DCS-1895 Refactor mandatory check integration test

--- licences-specs/src/test/groovy/uk/gov/justice/digital/hmpps/licences/specs/decision/MandatoryCheckSpec.groovy
+++ licences-specs/src/test/groovy/uk/gov/justice/digital/hmpps/licences/specs/decision/MandatoryCheckSpec.groovy
@@ -3,9 +3,7 @@ package uk.gov.justice.digital.hmpps.licences.specs.decision
 import geb.spock.GebReportingSpec
 import spock.lang.Shared
 import spock.lang.Stepwise
-import uk.gov.justice.digital.hmpps.licences.pages.TaskListPage
 import uk.gov.justice.digital.hmpps.licences.pages.decision.ApprovalMandatoryCheckPage
-import uk.gov.justice.digital.hmpps.licences.pages.decision.ApprovalRefusePage
 import uk.gov.justice.digital.hmpps.licences.pages.decision.ApprovalReleasePage
 import uk.gov.justice.digital.hmpps.licences.util.Actions
 import uk.gov.justice.digital.hmpps.licences.util.TestData
@@ -27,18 +25,18 @@ class MandatoryCheckSpec extends GebReportingSpec {
     actions.logOut()
   }
 
-  def 'Mandatory Check warning shown when mandatory risk check had not been completed '() {
+  def 'Mandatory check page is displayed before approval release page when Mandatory risk check has not been completed'() {
 
     given: 'Mandatory check not complete'
     testData.loadLicence('decision/mandatory-check-required')
 
     when: 'I view the mandatory check page'
-    at ApprovalMandatoryCheckPage, testData.markAndrewsBookingId
+    to ApprovalMandatoryCheckPage, testData.markAndrewsBookingId
 
-    and: 'I click the return button'
-    $('#backBtn').click()
+    and: 'I click the continue button'
+    $('#continueBtn').click()
 
-    then: 'I go back to the tasklist page'
-    at TaskListPage
+    then: 'I go to the approval release page'
+    at ApprovalReleasePage
   }
 }
