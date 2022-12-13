43be8ffcd4cad9088645e9b8ba767ce530502467
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-12-13 10:39:51 +0000

    code review changes

--- migrations/20221208172816_add-riskManagement-init-value.js
+++ migrations/20221208172816_add-riskManagement-init-value.js
@@ -1,6 +1,6 @@
 exports.up = async function up(knex) {
   await knex.schema.raw(`
-    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','1')
+    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','"1"')
     where licence -> 'risk' -> 'riskManagement' -> 'version' is null
 `)
 }
@@ -8,6 +8,6 @@ exports.up = async function up(knex) {
 exports.down = async function down(knex) {
   await knex.schema.raw(`
     update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','null')
-    where licence -> 'risk' -> 'riskManagement' -> 'version' = 1
+    where licence -> 'risk' -> 'riskManagement' -> 'version' = '"1"'
 `)
 }
