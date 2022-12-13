1dbe58659694e60e29884a86fa49e02954d30ad5
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2022-12-13 11:57:55 +0000

    DCS-1945 adding initial value for riskManagement version (#1405)
    * DCS-1945 adding initial value for riskManagement version

---
+++ migrations/20221208172816_add-riskManagement-init-value.js
@@ -0,0 +1,13 @@
+exports.up = async function up(knex) {
+  await knex.schema.raw(`
+    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','"1"')
+    where licence -> 'risk' -> 'riskManagement' -> 'version' is null
+`)
+}
+
+exports.down = async function down(knex) {
+  await knex.schema.raw(`
+    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','null')
+    where licence -> 'risk' -> 'riskManagement' -> 'version' = '"1"'
+`)
+}
