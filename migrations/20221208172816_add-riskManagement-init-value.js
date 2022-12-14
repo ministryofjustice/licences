60edeb865c35a3c5ffd0709f2044688d018e8262
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2022-12-14 11:49:42 +0000

    DCS-1958 migration script refactoring

--- migrations/20221208172816_add-riskManagement-init-value.js
+++ migrations/20221208172816_add-riskManagement-init-value.js
@@ -1,13 +1,13 @@
 exports.up = async function up(knex) {
   await knex.schema.raw(`
-    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','"1"')
-    where licence -> 'risk' -> 'riskManagement' -> 'version' is null
+    update licences set licence = licence #- '{risk, riskManagement, version}'
+    where licence -> 'risk' -> 'riskManagement' ->> 'version' = '"1"'
 `)
 }
 
 exports.down = async function down(knex) {
   await knex.schema.raw(`
-    update licences set licence = jsonb_set(licence,'{risk, riskManagement, version}','null')
-    where licence -> 'risk' -> 'riskManagement' -> 'version' = '"1"'
+    update licences set licence = licence #- '{risk, riskManagement, version}'
+    where licence -> 'risk' -> 'riskManagement' ->> 'version' = '"1"'
 `)
 }
