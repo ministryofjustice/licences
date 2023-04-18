6708fcea38b206333076eb96ef8239f79e8e5fa8
Louise Norris <louise.norris@digital.justice.gov.uk>
2023-04-18 16:48:33 +0100

    DCS-2182 bass referral offer page content changes

--- test/routes/bassReferral.test.js
+++ test/routes/bassReferral.test.js
@@ -22,7 +22,7 @@ describe('/hdc/bassReferral', () => {
 
       const routes = [
         { url: '/hdc/bassReferral/bassRequest/1', content: 'Does the offender have a preferred CAS2 area' },
-        { url: '/hdc/bassReferral/bassOffer/1', content: 'BASS address' },
+        { url: '/hdc/bassReferral/bassOffer/1', content: 'CAS2 address' },
         { url: '/hdc/bassReferral/rejected/1', content: 'BASS area rejected' },
         { url: '/hdc/bassReferral/unsuitable/1', content: 'Unsuitable for BASS' },
         { url: '/hdc/bassReferral/bassWithdrawn/1', content: 'BASS withdrawn' },
