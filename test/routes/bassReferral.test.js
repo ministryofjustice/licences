99b114d4b92a5c20bd65dbe57d24e4d277e6beaf
Louise Norris <louise.norris@digital.justice.gov.uk>
2023-04-18 17:00:12 +0100

    DCS-2183 unsuitable for cas2 content change

--- test/routes/bassReferral.test.js
+++ test/routes/bassReferral.test.js
@@ -24,7 +24,7 @@ describe('/hdc/bassReferral', () => {
         { url: '/hdc/bassReferral/bassRequest/1', content: 'Does the offender have a preferred CAS2 area' },
         { url: '/hdc/bassReferral/bassOffer/1', content: 'CAS2 address' },
         { url: '/hdc/bassReferral/rejected/1', content: 'BASS area rejected' },
-        { url: '/hdc/bassReferral/unsuitable/1', content: 'Unsuitable for BASS' },
+        { url: '/hdc/bassReferral/unsuitable/1', content: 'Unsuitable for CAS2' },
         { url: '/hdc/bassReferral/bassWithdrawn/1', content: 'BASS withdrawn' },
       ]
 
