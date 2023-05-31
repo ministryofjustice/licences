0c4b08974f02cfbf2673e33d1b8f85db713a497b
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-05-31 12:56:22 +0100

    DCS-2249 add migration script to set version value for curfew address… (#1481)
    * DCS-2249 add migration script to set version value for curfew address review question for active/started licences
    
    * DCS-2249 add missing brace

---
+++ migrations/20230530092632_add-curfewAddressReview-version-init-value.js
@@ -0,0 +1,13 @@
+exports.up = async function up(knex) {
+  await knex.schema.raw(`
+      update licences set licence = jsonb_set(licence,'{curfew, curfewAddressReview, version}','"1"')
+      where licence -> 'curfew' -> 'curfewAddressReview' ->> 'version' is null
+  `)
+}
+
+exports.down = async function down(knex) {
+  await knex.schema.raw(`
+      update licences set licence = licence #- '{curfew, curfewAddressReview, version}'
+      where licence -> 'curfew' -> 'curfewAddressReview' ->> 'version' = '"1"'
+  `)
+}
