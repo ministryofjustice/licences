698ec7183c3b1dd412e56eb499ebf4d6aac81cca
Louise Norris <louise.norris@digital.justice.gov.uk>
2023-08-17 16:56:32 +0100

    CVSL-1255 remove comment

--- server/services/config/conditionsConfig.ts
+++ server/services/config/conditionsConfig.ts
@@ -2,7 +2,6 @@ import { AdditionalConditionsVersion, ConditionMetadata } from '../../data/licen
 import { AdditionalConditions } from '../../data/licenceTypes'
 import * as additionalConditionsV1 from './conditions/additionalConditions/v1/conditions'
 import * as additionalConditionsV2 from './conditions/additionalConditions/v2/conditions'
-// import { standardConditions as stdConditionsV1 } from './conditions/standardConditions/v1/standardConditions'
 import { standardConditions as stdConditionsV2 } from './conditions/standardConditions/v2/standardConditions'
 
 export const standardConditions = stdConditionsV2
