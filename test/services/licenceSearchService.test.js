ae80ace4bd769832d0023eb01660cb83ec2eccda
Louise Norris <louise.norris@digital.justice.gov.uk>
2023-10-10 11:31:04 +0100

    CVSL-1370 fix test and filter out inactive prisoners from com licences csv

--- test/services/licenceSearchService.test.js
+++ test/services/licenceSearchService.test.js
@@ -1,4 +1,3 @@
-const moment = require('moment')
 const createLicenceSearchService = require('../../server/services/licenceSearchService')
 
 let licenceSearchService
@@ -14,9 +13,9 @@ describe('licenceSearchService', () => {
     licenceClient = {
       getLicence: jest.fn(),
       getLicencesInStage: jest.fn().mockReturnValue([
-        { booking_id: 1, transitionDate: '01-01-2020' },
-        { booking_id: 2, transitionDate: '01-01-2020' },
-        { booking_id: 3, transitionDate: '01-01-2020' },
+        { booking_id: 1, transition_date: '01-01-2020' },
+        { booking_id: 2, transition_date: '01-01-2020' },
+        { booking_id: 3, transition_date: '01-01-2020' },
       ]),
     }
 
@@ -30,9 +29,27 @@ describe('licenceSearchService', () => {
 
     prisonerSearchAPI = {
       getPrisoners: jest.fn().mockReturnValue([
-        { bookingId: '1', prisonerNumber: 'AAAA11', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '2', prisonerNumber: 'AAAA12', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '3', prisonerNumber: 'AAAA13', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
+        {
+          bookingId: '1',
+          prisonerNumber: 'AAAA11',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+        },
+        {
+          bookingId: '2',
+          prisonerNumber: 'AAAA12',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+        },
+        {
+          bookingId: '3',
+          prisonerNumber: 'AAAA13',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+        },
       ]),
     }
 
@@ -113,19 +130,37 @@ describe('licenceSearchService', () => {
       )
     })
 
-    test('should not add released prisoners (with no matching bookingId from licences) to csv string', async () => {
+    test('should not add released prisoners to csv string', async () => {
       prisonerSearchAPI.getPrisoners.mockReturnValue([
-        { bookingId: '1', prisonerNumber: 'AAAA11', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '2', prisonerNumber: 'AAAA12', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '3', prisonerNumber: 'AAAA13', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '4', prisonerNumber: 'AAAA14', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
-        { bookingId: '5', prisonerNumber: 'AAAA15', prisonId: 'MDI', homeDetentionCurfewEligibilityDate: '01-01-2021' },
+        {
+          bookingId: '1',
+          prisonerNumber: 'AAAA11',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+        },
+        {
+          bookingId: '2',
+          prisonerNumber: 'AAAA12',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+        },
+        {
+          bookingId: '3',
+          prisonerNumber: 'AAAA13',
+          prisonId: 'MDI',
+          prisonName: 'Moorland (HMP & YOI)',
+          homeDetentionCurfewEligibilityDate: '01-01-2021',
+          status: 'INACTIVE OUT',
+        },
       ])
       const result = await licenceSearchService.getLicencesInStageCOM('user-1')
 
       expect(result).toContain(
-        'PRISON_NUMBER,PRISON_ID,TRANSITION_DATE,HDCED\nAAAA11,MDI,09-10-2023,01-01-2021\nAAAA12,MDI,09-10-2023,01-01-2021\nAAAA13,MDI,09-10-2023,01-01-2021'
+        'PRISON_NUMBER,PRISON_ID,PRISON_NAME,HANDOVER_DATE,HDCED\nAAAA11,MDI,Moorland (HMP & YOI),01-01-2020,01-01-2021\nAAAA12,MDI,Moorland (HMP & YOI),01-01-2020,01-01-2021'
       )
+      expect(result).not.toContain('AAAA13,MDI,Moorland (HMP & YOI),01-01-2020,01-01-2021')
     })
   })
 })
