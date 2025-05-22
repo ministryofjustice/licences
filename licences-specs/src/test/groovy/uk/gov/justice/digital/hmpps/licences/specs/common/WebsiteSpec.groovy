d2cea21a161a63127a694b8d127571e976bc45d6
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2025-05-22 09:20:25 +0100

    CVSL-2890 fix failing test

--- licences-specs/src/test/groovy/uk/gov/justice/digital/hmpps/licences/specs/common/WebsiteSpec.groovy
+++ licences-specs/src/test/groovy/uk/gov/justice/digital/hmpps/licences/specs/common/WebsiteSpec.groovy
@@ -37,7 +37,7 @@ class WebsiteSpec extends GebReportingSpec {
 
     where:
     user       | displayName
-    'CA'       | 'W. Gul'
+    'CA'       | 'K Haz'
     'DM'       | 'H. Fann'
     'READONLY' | 'L. Only'
     'RO'       | 'O. Hanten'
