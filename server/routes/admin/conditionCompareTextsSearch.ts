654f7a40b22afe2cb524bc01ac1397ed7c020ec1
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-02-19 12:10:15 +0000

    Cvsl 3791 extra tweaks filtering (#1652)
    * CVSL-3810 improved more efficient SQL
    
    * CVSL-3810 improved tweaks and filtering options
    
    * CVSL-3810 improved tweaks and filtering options
    
    * CVSL-3810 improved tweaks and filtering options
    
    * CVSL-3810 improved tweaks and filtering options
    
    * CVSL-3810 improved tweaks and filtering options

--- server/routes/admin/conditionCompareTextsSearch.ts
+++ server/routes/admin/conditionCompareTextsSearch.ts
@@ -23,18 +23,25 @@ function getIdsFromReq(req: Request) {
   const versionRaw = getSingleValue(req.body?.version ?? req.query?.version)
   const version = versionRaw ? Number(versionRaw) : undefined
 
+  const cleanUiRaw = getSingleValue(req.body?.cleanUi ?? req.query?.cleanUi)
+  const cleanApiRaw = getSingleValue(req.body?.cleanApi ?? req.query?.cleanApi)
+
+  const cleanUi = cleanUiRaw === 'TRUE' || cleanUiRaw === 'ON'
+  const cleanApi = cleanApiRaw === 'TRUE' || cleanApiRaw === 'ON'
+
   if (req.body?.action === 'Next') {
     const rangeSize = idTo - idFrom
     idFrom = idTo + 1
     idTo = idFrom + rangeSize
   }
 
-  return { idFrom, idTo, codeFilter, version }
+  logger.info({ idFrom, idTo, codeFilter, version, cleanUi, cleanApi, cleanUiRaw, cleanApiRaw }, 'Search params')
+  return { idFrom, idTo, codeFilter, version, cleanUi, cleanApi }
 }
 
 async function fetchLicenceResults(hdcService: HdcService, req: Request): Promise<LicenceDiff[]> {
-  const { idFrom, idTo, codeFilter, version } = getIdsFromReq(req)
-  return hdcService.compareConditions(idFrom, idTo, codeFilter, version)
+  const { idFrom, idTo, codeFilter, version, cleanUi, cleanApi } = getIdsFromReq(req)
+  return hdcService.compareConditions(idFrom, idTo, codeFilter, version, cleanUi, cleanApi)
 }
 
 export = (hdcService: HdcService) => (router: Router, audited: any) => {
@@ -57,9 +64,7 @@ export = (hdcService: HdcService) => (router: Router, audited: any) => {
       // Reset
       req.session!.searchResults = undefined
 
-      const { idFrom, idTo, codeFilter, version } = getIdsFromReq(req)
-
-      logger.info({ idFrom, idTo, count: result.length }, 'GET search for licences')
+      const { idFrom, idTo, codeFilter, version, cleanUi, cleanApi } = getIdsFromReq(req)
 
       return res.render('admin/licences/conditionCompareTextsSearch', {
         errors,
@@ -68,6 +73,8 @@ export = (hdcService: HdcService) => (router: Router, audited: any) => {
         idTo,
         codeFilter,
         version,
+        cleanUi,
+        cleanApi,
       })
     })
   )
@@ -77,7 +84,7 @@ export = (hdcService: HdcService) => (router: Router, audited: any) => {
     audited,
     asyncMiddleware(async (req, res) => {
       const licenceDiffs = await fetchLicenceResults(hdcService, req)
-      let { idFrom, idTo, codeFilter, version } = getIdsFromReq(req)
+      let { idFrom, idTo, codeFilter, version, cleanUi, cleanApi } = getIdsFromReq(req)
 
       if (!licenceDiffs.length) {
         req.flash('errors', { id: `Could not find mismatched conditions` })
@@ -86,7 +93,14 @@ export = (hdcService: HdcService) => (router: Router, audited: any) => {
       // Store results
       req.session!.searchResults = licenceDiffs
 
-      const queryParams = `?idFrom=${idFrom}&idTo=${idTo}${codeFilter ? `&codeFilter=${codeFilter}` : ''} ${version ? `&version=${version}` : ''}`
+      const queryParams =
+        `?idFrom=${idFrom}` +
+        `&idTo=${idTo}` +
+        `${codeFilter ? `&codeFilter=${codeFilter}` : ''}` +
+        `${version ? `&version=${version}` : ''}` +
+        `${cleanUi ? `&cleanUi=true` : ''}` +
+        `${cleanApi ? `&cleanApi=true` : ''}`
+
       return res.redirect(`/admin/conditionCompareTextsSearch${queryParams}`)
     })
   )
