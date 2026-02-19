6b2c350f57529f655a7bd8d717610a057df6abc2
AledWEvans <aled.evans1@justice.gov.uk>
2026-02-19 11:06:34 +0000

    CVSL-3810 improved tweaks and filtering options

--- server/services/hdc/hdcService.ts
+++ server/services/hdc/hdcService.ts
@@ -145,7 +145,7 @@ export class HdcService {
     }
 
     if (code === 'DRUG_TESTING') {
-      out = out.replace(/^Attend,/, 'Attend')
+      //out = out.replace(/^Attend,/, 'Attend')
     }
 
     // Collapse extra spaces
@@ -168,7 +168,7 @@ export class HdcService {
     out = out.normalize('NFKC')
 
     if (code === 'DRUG_TESTING') {
-      out = out.replace(/^Attend,/, 'Attend')
+      // out = out.replace(/^Attend,/, 'Attend')
     }
 
     // Collapse extra spaces
