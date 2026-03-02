ab1b5a27f139b6314c2b12d096c465dece520f8f
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2026-03-02 12:16:01 +0000

    CVSL-3878 dependency updates (#1660)
    * CVSL-3878 dependency updates
    
    * Fixing tests

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
-    'node_modules/@scarf/scarf@1.4.0': 'ALLOW',
+    'node_modules/@scarf/scarf@1.4.0': 'FORBID',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
