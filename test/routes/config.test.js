7ab5b082b12f7928cdc45c90bd4ffdf3732b56eb
Aled Evans <Aled.Evans1@justice.gov.uk>
2026-06-26 14:13:59 +0100

    CVSL-4250 PROG HDC - VARY National role out config base date toggle (#1728)
    * CVSL-4249 PROG HDC - VARY EA - Replace “Change” links with “Vary licence” row for licences with existing variations
    
    * CVSL-4249 PROG HDC - VARY EA - Replace “Change” links with “Vary licence” row for licences with existing variations
    
    * changes after review

---
+++ test/routes/config.test.js
@@ -0,0 +1,85 @@
+describe('hdcInCvlNationalRoleOut Config', () => {
+  const originalEnv = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...originalEnv }
+  })
+
+  afterEach(() => {
+    process.env = originalEnv
+  })
+
+  const loadConfig = () => {
+    let config
+
+    jest.isolateModules(() => {
+      // eslint-disable-next-line global-require
+      config = require('../../server/config').default
+    })
+
+    if (!config) {
+      throw new Error('Config was not loaded')
+    }
+
+    return /** @type {any} */ (config)
+  }
+
+  it('should be active when the rollout date is in the past', () => {
+    // Given
+    process.env.HDC_IN_CVL_NATIONAL_ROLE_OUT_DATE = '2000-01-01'
+
+    // When
+    const config = loadConfig()
+
+    // Then
+    expect(config.hdcInCvlNationalRoleOut.isActive()).toBe(true)
+  })
+
+  it('should not be active when the rollout date is in the future', () => {
+    // Given
+    process.env.HDC_IN_CVL_NATIONAL_ROLE_OUT_DATE = '2999-01-01'
+
+    // When
+    const config = loadConfig()
+
+    // Then
+    expect(config.hdcInCvlNationalRoleOut.isActive()).toBe(false)
+  })
+
+  it('should have a null and not enabled roleOutDate when the env var is not set', () => {
+    // Given
+    delete process.env.HDC_IN_CVL_NATIONAL_ROLE_OUT_DATE
+
+    // When
+    const config = loadConfig()
+
+    // Then
+    expect(config.hdcInCvlNationalRoleOut.roleOutDate).toBeNull()
+    expect(config.hdcInCvlNationalRoleOut.isActive()).toBe(false)
+  })
+
+  it('should have a null and not enabled when the env var is not a date', () => {
+    // Given
+    process.env.HDC_IN_CVL_NATIONAL_ROLE_OUT_DATE = 'some-invalid-date'
+
+    // When
+    const config = loadConfig()
+
+    // Then
+    expect(config.hdcInCvlNationalRoleOut.roleOutDate).toBeNull()
+    expect(config.hdcInCvlNationalRoleOut.isActive()).toBe(false)
+  })
+
+  it('should be active when the rollout date is today', () => {
+    // Given
+    const [today] = new Date().toISOString().split('T')
+    process.env.HDC_IN_CVL_NATIONAL_ROLE_OUT_DATE = today
+
+    // When
+    const config = loadConfig()
+
+    // Then
+    expect(config.hdcInCvlNationalRoleOut.isActive()).toBe(true)
+  })
+})
